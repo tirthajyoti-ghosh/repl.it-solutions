f1388190896534612f565dcbfd25b232d874e61e
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-21 16:11:17 +0530

    6.1 Sets

---
+++ 6.1_sets.rb
@@ -0,0 +1,22 @@
+require 'set'
+
+def find_duplicates(array)
+  set = Set.new
+  duplicates = []
+  
+  array.each do |e|
+  	if set.include?(e)
+  		duplicates << e
+  	else
+  		set << (e)
+  	end
+  end
+  
+  duplicates
+end
+
+p find_duplicates([1, 2, 3, 1, 5, 6, 7, 8, 5, 2])
+# => [1, 5, 2]
+
+p find_duplicates([3, 501, 17, 23, -43, 67, 5, 888, -402, 235, 77, 99, 311, 1, -43])
+# => [-43]
